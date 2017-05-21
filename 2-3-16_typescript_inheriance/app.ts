bd1dedb5215ffeed31f24a66d23c7c3f2fa7d1ad
Iqra Khalil <ikk_9@yahoo.com>
2017-05-21 09:03:12 -0700

    commit

---
+++ 2-3-16_typescript_inheriance/app.ts
@@ -0,0 +1,108 @@
+
+
+class employee {
+      id:string;
+     name:string;
+	  constructor (id:string , name:string)
+	  {	  this.id = id;
+		  this.name = name;
+	  }
+	  print()
+	 {   
+	 console.log("The employe name is : "+ this.id);
+	console.log("The employe name is : "+ this.name);
+	 }
+}
+var emp:employee  = new employee("E121001c", "ahmed");
+emp.print();	
+
+/*class employee {
+      id:string;
+	  name:string;
+	  
+	  
+	  constructor (id:string , name:string)
+	  {
+		  this.id = id;
+		  this.name = name;
+	  }
+	  
+	 print()
+	 {   
+		 console.log("The employe name is : "+ this.id);
+		 console.log("The employe name is : "+ this.name);
+	 }
+}
+
+class department extends employee {
+	 
+	 department : string;
+	 designation: string;
+	 
+	 
+/*	 constructor (dept:string , des:string )
+	 {
+		 this.department = dept;
+		 this.designation = des;
+		  }
+	 
+*/	 
+	/*  print1()
+	 {   
+		// console.log("The employe name is : "+ super(id));
+		 console.log("The employe name is : "+ this.department);
+		 console.log("The employe name is : "+ this.designation);
+	 }
+}
+
+
+var emp:employee  = new employee("E121001c", "ahmed");
+var depart:department = new department("sds232" , "cs" ),
+
+
+emp.print();
+
+depart.print1();	
+	
+*/	
+function click1(){
+	alert ("Hello World");
+}
+
+function save(){
+
+	var name : string = (<HTMLInputElement>document.getElementById('name')).value;
+	document.write(name);
+}
+
+/* var two:number= 2;
+var ten:number = 10;
+
+
+
+var result:number = two + ten;
+document.writeln("two plus ten = " +result + "\n");
+var linebreak:string = "<br />"
+var temp:number = 12+result;
+document.write("12+12 is:" +temp);
+
+//console.info(result);
+//document.write(linebreak);
+/*
+document.write("ten * ten = ")
+result = ten * ten
+document.write(result)
+document.write(linebreak)
+
+document.write("ten / two = ")
+result = ten / two
+document.write(result)
+
+
+*/	
+	
+	
+
+	
+	
+
